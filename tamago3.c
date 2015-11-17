86e58535085e6bab47f23c4d1736265db6d88c5b
Moritama <moritama1515@gmail.com>
2015-11-17 14:34:34 +0900

    add comment

--- tamago3.c
+++ tamago3.c
@@ -13,25 +13,48 @@
 /*------------------------------------------------------*/
 /* 変数                                                 */
 /*------------------------------------------------------*/
+
+/* 碁盤 */
 int board[ALLBOARD] = {};
+/* MARK用の碁盤 */
 int check_board[ALLBOARD] = {};
+/* 着手場所の周りを調べる(右から時計回り) */
 int dir4[4] = {+1,+WIDTH,-1,-WIDTH};
 int dir8[8] = {+1,+1+WIDTH,+WIDTH,-1+WIDTH,-1,-1-WIDTH,-WIDTH,+1-WIDTH};   
-int color,z,prisoner_b,prisoner_w;
+/* 手盤 */
+int color;
+/* 着手場所 */
+int z;
+/* アゲハマ */
+int prisoner_b,prisoner_w;
 
 /*-------------------------------------------------------*/
 /* 関数                                                  */
 /*-------------------------------------------------------*/
-void CheckBoard(void);        // 盤面(board)の表示
-void CheckBoard_check(void);  // check_boardの表示
-void BoardIni(void);          // 盤面(board)の初期化
-void ClearCheckBoard(void);   // check_boardの初期化
-int CheckPut(int x,int y);    // 石を置く
-int FlipColor(int color);     // 色(手番)の反転
-int CheckWarning(int z);      // 自殺手かどうか調べる
-int TakeStone(int z);         // zの石が囲まれているか調べる
-int RemoveStone(int z);       // TakeStoneがTRUEならばRemoveCountを呼び出す
-int RemoveCount(int z);       // 囲まれた石を取り除きアゲハマ(prisoner)を返す
+
+/* 盤面(board)の表示 */
+void CheckBoard(void);
+/* check_boardの表示 */
+void CheckBoard_check(void);
+/* 盤面(board)の初期化 */
+void BoardIni(void);
+/* check_boardの初期化 */
+void ClearCheckBoard(void);
+/* 石を置く */
+int CheckPut(int x,int y);
+/* 色(手番)の反転 */
+int FlipColor(int color);
+/* 自殺手かどうか調べる */
+int CheckWarning(int z);
+/* zの石が囲まれているか調べる */
+int TakeStone(int z);
+/* TakeStoneがTRUEならばRemoveCountを呼び出す */
+int RemoveStone(int z);
+/* 囲まれた石を取り除きアゲハマ(prisoner)を返す */
+int RemoveCount(int z);
+
+
+
 
 void CheckBoard(void)
 {
